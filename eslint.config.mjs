3ca9b2addf89504a3992b210724c7ca2bb1be422
alexx-kis <kislov@advline.ru>
2025-01-28 17:13:49 +0300

    fix: eslint config modified, it works

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.config({
-    extends: ['next'],
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
     rules: {
-      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
-  }),
+  },
 ];
 
 export default eslintConfig;
