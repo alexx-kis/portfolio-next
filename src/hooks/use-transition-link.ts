58317d29376ae64ee63d6f6172ec7e8aa992cd2d
alexx-kis <kislov@advline.ru>
2025-01-30 12:43:05 +0300

    fix: use transition on same path

--- src/hooks/use-transition-link.ts
+++ src/hooks/use-transition-link.ts
@@ -1,13 +1,17 @@
 import { PAGE_TRANSITION_TIME } from '@/constants/const';
 import { executeTransition } from '@/utils/utils';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 // %======================== useTransition ========================% //
 
 export const useTransitionLink = () => {
   const router = useRouter();
+  const pathname = usePathname();
 
   const handleTransition = async (e: React.MouseEvent<HTMLAnchorElement>, href: string) => {
+    if (pathname === href) {
+      return;
+    }
     e.preventDefault();
     document.body.classList.add('page-transition');
     await executeTransition(PAGE_TRANSITION_TIME);
